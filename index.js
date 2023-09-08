fa6f4c5ac617ef63cbc3a991736f51e7f87d66ab
StevieG46 <126635670+StevieG46@users.noreply.github.com>
2023-09-09 09:43:13 +1000

    feat: example init
    no functionality yet

---
+++ index.js
@@ -0,0 +1,14 @@
+const { donwloadPokemonPicture } = require("./downloader.js");
+
+donwloadPokemonPicture().then(savedFileOutput => {
+    console.log("New image is saved to " + savedFileOutput)
+}).catch(error => {
+    console.log(error);
+})
+
+async function exampleDownload(){
+    let savedFileOutput = await donwloadPokemonPicture();
+    console.log("New image is async savbed to: " + savedFileOutput)
+}
+
+exampleDownload();
